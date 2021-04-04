f1d047392a06773e3763c1bc7cb4631cb3346e0b
Franck Martinez <franck.martinez@pole-emploi.fr>
2021-04-04 10:15:55 +0200

    first commit

---
+++ src/main/java/com/tatoo/virginie/VirginieApplication.java
@@ -0,0 +1,13 @@
+package com.tatoo.virginie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirginieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirginieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tatoo/virginie/VirginieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatoo.virginie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirginieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
